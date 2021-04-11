465ee1782e2a165bbe0a4d2d90a2cacc7eb8a674
vasnastos <nastosvasileios99@gmail.com>
2021-04-11 11:48:42 +0300

    update file_walk

--- file_walk.py
+++ file_walk.py
@@ -8,7 +8,7 @@ def file_parser():
     try:
         for path,_,file in os.walk(top=parentroot,topdown=True):
             allfiles+=[path+'/'+l for l in file if l.endswith('.cpp') or l.endswith('.c') or l.endswith('.hpp') or l.endswith('.h')]
-    except FileNotFoundError:
+    except Exception:
         print('Can not open folder oop-master')
         return list([])
     return allfiles
