408ebb2d88382db913d691656840bc125290b4ea
vasnastos <nastosvasileios99@gmail.com>
2021-04-11 14:19:19 +0300

    update sample

--- 5.for_loops/for_over_12_ch.py
+++ 5.for_loops/for_over_12_ch.py
@@ -10,7 +10,7 @@ def over_12_characters():
     found=0
     print(len(files))
     #for regex
-    pattern=re.compile('for\(.*;.*;.*\)')
+    pattern=re.compile('for\(.*;.*;.*\)$|for\(.+:.+\)$')
     for x in files:
         with open(x,'r',encoding='ISO-8859-1') as f:
            for k in f:
@@ -19,5 +19,5 @@ def over_12_characters():
                    if len(current.replace(' ',''))>=12:
                          found+=1
     print(f'for loops with over 12 characters:{found}')
-
+   
 over_12_characters()
