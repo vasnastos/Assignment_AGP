80fead3ed729fc47ab9c37ac0e2b353d85355bec
vasnastos <nastosvasileios99@gmail.com>
2021-04-11 17:14:33 +0300

    updates

--- 5.for_loops/for_over_12_ch.py
+++ 5.for_loops/for_over_12_ch.py
@@ -3,21 +3,28 @@ sys.path.append('..')
 import file_walk as fw 
 import re
 import os
+import termcolor as tm
 
 files=fw.file_parser()
 
 def over_12_characters():
     found=0
     print(len(files))
+    print(os.getcwd())
     #for regex
-    pattern=re.compile('for\(.*;.*;.*\)$|for\(.+:.+\)$')
+    pattern=re.compile('for\s*\(.*\s*;.*\s*;.*\s*\)|for\s*\(.+\s*:.+\s*\)')
     for x in files:
         with open(x,'r',encoding='ISO-8859-1') as f:
            for k in f:
+               fnd=0
                for l in pattern.findall(k):
+                   fnd=1
                    current=l[int(l.find('('))+1:len(l)-1]
                    if len(current.replace(' ',''))>=12:
                          found+=1
+                        
+               tm.cprint(k,'green') if fnd==1 and 'for' in k else tm.cprint(k,'red') if 'for' in k else print('',end='')
+    
     print(f'for loops with over 12 characters:{found}')
    
 over_12_characters()
--- file_walk.py
+++ file_walk.py
@@ -1,6 +1,7 @@
 import os
+import re
 
-parentroot='../../oop-master'
+parentroot='../oop-master'
 
 def file_parser():
     print(os.getcwd())
@@ -12,4 +13,40 @@ def file_parser():
         print('Can not open folder oop-master')
         return list([])
     return allfiles
+
+files=file_parser()
+
+
+def filesbycategory():
+    counter={'cpp':0,'hpp':0,'h':0,'c':0}
+    for x in files:
+        if re.match('.+\.cpp$',x): 
+            #if x.endswith('.cpp'):
+            counter['cpp']+=1
+        elif x.endswith('.hpp'):
+            counter['hpp']+=1
+        elif x.endswith('.h'):
+            counter['h']+=1
+        else:
+            counter['c']+=1
+    
+    for key in counter:
+        print(str(key)+'-->'+str(counter[key]))
+
+
+def codelines():
+    linecounter=0
+    for i in files:
+        with open(i,'r') as f:
+            for j in f:
+                if len(j.strip())!=0:
+                    linecounter+=1
+    print('Lines of code:'+str(linecounter))
+
+
+
+  
+filesbycategory()
+codelines()
+print('=='*30)
 
